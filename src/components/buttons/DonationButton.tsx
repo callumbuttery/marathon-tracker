723dfb1b6536a77076437e84a66201082f1d5685
Callum Buttery <callumbuttery@icloud.com>
2023-06-06 23:18:24 +0100

    Fix: button styles

--- src/components/buttons/DonationButton.tsx
+++ src/components/buttons/DonationButton.tsx
@@ -1,7 +1,7 @@
 export const DonationButton: React.FC = () => {
     return (
         <div>
-            <button type="button" class="text-white bg-gradient-to-br from-pink-500 to-orange-400 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-pink-200 dark:focus:ring-pink-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2">Donate!</button>
+            <button type="button" className="animate-bounce shadow-4xl text-white bg-gradient-to-br from-pink-500 to-orange-400 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-pink-200 dark:focus:ring-pink-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2">Donate!</button>
         </div>
     )
 }
--- src/components/buttons/ViewEvent.tsx
+++ src/components/buttons/ViewEvent.tsx
@@ -1,7 +1,7 @@
 export const ViewEventButton: React.FC = () => {
     return (
         <div>
-            <button type="button" className="text-white bg-gradient-to-br from-purple-600 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2">View Event!</button>
+            <button type="button" className="shadow-4xl text-white bg-gradient-to-br from-purple-600 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2">View Event!</button>
         </div>
     )
 }
