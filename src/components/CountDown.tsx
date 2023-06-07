96e46c793c588d06dd793180fdfec49fceef8c83
Callum Buttery <callumbuttery@icloud.com>
2023-06-07 09:28:55 +0100

    Chore: clean up

--- src/components/CountDown.tsx
+++ src/components/CountDown.tsx
@@ -3,7 +3,6 @@ import moment from 'moment';
 
 export const CountDown: React.FC = () => {
 
-    const [seconds, setSeconds] = useState(0);
     const [minutes, setMinutes] = useState(0);
     const [hours, setHours] = useState(0);
     const [days, setDays] = useState(0);
