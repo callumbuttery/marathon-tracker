4577c3daac9508d65dab613e6e4ac364f05cf63b
Callum Buttery <callumbuttery@icloud.com>
2023-06-07 09:24:45 +0100

    Fix: update styling

--- src/components/CountDown.tsx
+++ src/components/CountDown.tsx
@@ -18,28 +18,28 @@ export const CountDown: React.FC = () => {
     });
 
     return (
-        <div className="flex justify-center items-center">
+        <div className="flex justify-center items-center mt-10 mb-10">
             <div className="grid grid-flow-col gap-5 text-center auto-cols-max">
                 <div className="flex flex-col p-2 bg-neutral rounded-box text-neutral-content">
-                    <span className="countdown font-mono text-7xl">
+                    <span className="countdown font-mono text-2xl">
                         <span>{Months}</span>
                     </span>
                     months
                 </div>
                 <div className="flex flex-col p-2 bg-neutral rounded-box text-neutral-content">
-                    <span className="countdown font-mono text-7xl">
+                    <span className="countdown font-mono text-2xl">
                         <span>{days}</span>
                     </span>
                     days
                 </div>
                 <div className="flex flex-col p-2 bg-neutral rounded-box text-neutral-content">
-                    <span className="countdown font-mono text-7xl">
+                    <span className="countdown font-mono text-2xl">
                         <span>{hours}</span>
                     </span>
                     hours
                 </div>
                 <div className="flex flex-col p-2 bg-neutral rounded-box text-neutral-content">
-                    <span className="countdown font-mono text-7xl">
+                    <span className="countdown font-mono text-2xl">
                         <span>{minutes}</span>
                     </span>
                     min
--- src/pages/Landing.tsx
+++ src/pages/Landing.tsx
@@ -9,13 +9,13 @@ import { mdiFlagCheckered, mdiRun } from '@mdi/js';
 export const Landing: React.FC = () => {
     return (
         <div className="h-screen bg-[radial-gradient(ellipse_at_center,_var(--tw-gradient-stops))] from-sky-400 to-blue-500 pt-72">
-            <CountDown />
 
-            <div className="flex justify-center items-center mt-10 font-mono text-xl">
+            <div className="flex justify-center items-center mt-10 font-mono text-4xl">
                 <Icon path={mdiRun} size={1} color={'white'} />
                 <span className="ml-2 mr-2">Edinburgh Half Marathon 2023</span>
                 <Icon path={mdiFlagCheckered} size={1.5} color={'white'} />
             </div>
+            <CountDown />
             <div className="flex justify-center mt-5">
                 <DonationButton />
                 <ViewEventButton />
