628ea5ff553b77d1ce89b4f59d88d6ca091d0a73
Callum Buttery <callumbuttery@icloud.com>
2023-06-08 17:37:02 +0100

    Feat: set up strava api call

--- src/pages/Landing.tsx
+++ src/pages/Landing.tsx
@@ -3,41 +3,37 @@ import { DonationButton } from "../components/buttons/DonationButton"
 import { ViewEventButton } from "../components/buttons/ViewEvent"
 import { ProgressBar } from "../components/loading/ProgressBar";
 import { ScrollAnimator } from "../components/ScrollAnimator";
+import { Training } from "./Training";
 
 import Icon from '@mdi/react';
 import { mdiFlagCheckered, mdiRun } from '@mdi/js';
-import axios from 'axios';
 
-export async function Landing(){
-
-    const data = await axios({
-        method: 'GET',
-        url: `https://www.strava.com/api/v3/athlete/activities?access_token=${import.meta.env.VITE_STRAVA_KEY}`,
-      });
-
-    console.log('data: ', data);
+export const Landing: React.FC = () => {
 
     return (
-        <div className="h-screen bg-gradient-to-tl from-indigo-700 to-purple-700 pt-52">
-            <div className="w-6/12 mx-auto rounded-2xl pb-2 pt-1 border-2">
-                <CountDown />
-                <div className="flex justify-center items-center font-mono text-xl mt-12 text-white">
-                    <Icon path={mdiRun} size={1} color={'white'} />
-                    <span className="ml-2 mr-2">Edinburgh Half Marathon 2023</span>
-                    <Icon path={mdiFlagCheckered} size={1.5} color={'white'} />
+        <div>
+            <div className="h-screen bg-gradient-to-tl from-indigo-700 to-purple-700 pt-52">
+                <div className="w-6/12 mx-auto rounded-2xl pb-2 pt-1 border-2">
+                    <CountDown />
+                    <div className="flex justify-center items-center font-mono text-xl mt-12 text-white">
+                        <Icon path={mdiRun} size={1} color={'white'} />
+                        <span className="ml-2 mr-2">Edinburgh Half Marathon 2023</span>
+                        <Icon path={mdiFlagCheckered} size={1.5} color={'white'} />
+                    </div>
+                    <div className="flex justify-center items-center font-mono text-xl text-white">
+                        24.09.23
+                    </div>
+                    <div className="flex justify-center mt-6">
+                        <DonationButton />
+                        <ViewEventButton />
+                    </div>
+                    <div className="mt-4 mb-4">
+                        <ProgressBar />
+                    </div>
                 </div>
-                <div className="flex justify-center items-center font-mono text-xl text-white">
-                    24.09.23
-                </div>
-                <div className="flex justify-center mt-6">
-                    <DonationButton />
-                    <ViewEventButton />
-                </div>
-                <div className="mt-4 mb-4">
-                    <ProgressBar />
-                </div>
+                <ScrollAnimator />
             </div>
-            <ScrollAnimator />
+            <Training/>
         </div>
     )
 }
---
+++ src/pages/Training.tsx
@@ -0,0 +1,43 @@
+import { StravaCard } from "../components/sections/StravaCard"
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+
+export const Training: React.FC = () => {
+
+    const [stravaData, setStravaData] = useState([]);
+
+    useEffect(() => {
+
+        async function fetchStravaData() {
+            try {
+                let response = await axios(`https://www.strava.com/api/v3/athlete/activities?access_token=${import.meta.env.VITE_STRAVA_KEY}`, {
+                    method: 'GET',
+                })
+
+                if(!response.data) {
+                    throw new Error('Failed to get strava data');
+                }
+
+                console.log('response: ', response.data)
+                setStravaData(response.data);
+            } catch (e) {
+                alert(e);
+            }
+        }
+
+        fetchStravaData();
+    }, []);
+
+    return (
+        <div className="h-screen bg-gradient-to-tl from-blue-700 to-purple-700 pt-32">
+            <h1 className="text-6xl flex justify-center text-white font-mono tracking-widest">TRAINING DIARY</h1>
+            <div className="flex justify-center mt-32">
+                {
+                    stravaData.map(({activity}: any)=> {
+                        return <StravaCard />
+                    })
+                }
+            </div>
+        </div>
+    )
+}
