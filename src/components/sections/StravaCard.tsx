14540cb3ffbb39e180c4a7ab294021b6ca046cf7
Callum Buttery <callumbuttery@icloud.com>
2023-06-08 19:09:28 +0100

    Fix: working object passed as props

--- src/components/sections/StravaCard.tsx
+++ src/components/sections/StravaCard.tsx
@@ -1,10 +1,11 @@
-interface stravaProps {
-    dataObj: object,
-    key: number,
+import { StavaData } from "../../interfaces/StravaData";
+
+interface expectedProps {
+    obj: StavaData
 }
 
-export const StravaCard: React.FC<stravaProps> = ( item ) => {
-    console.log(item);
+export const StravaCard: React.FC<expectedProps> = (props) => {
+    console.log('dataobj: ', props.obj.name);
     return (
         <div className="w-96 ml-10 mt-24 drop-shadow-4xl hover:scale-110 hover:duration-300">
             <div className="h-96 max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
@@ -13,7 +14,7 @@ export const StravaCard: React.FC<stravaProps> = ( item ) => {
                 </a>
                 <div className="p-5">
                     <a href="#">
-                        <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Noteworthy technology acquisitions 2021</h5>
+                        <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">test</h5>
                     </a>
                     <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.</p>
                     <a href="#" className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
--- src/pages/Training.tsx
+++ src/pages/Training.tsx
@@ -29,12 +29,12 @@ export const Training: React.FC = () => {
     }, []);
 
     return (
-        <div className="h-screen bg-gradient-to-tl from-blue-700 to-purple-700 pt-32">
+        <div className="h-full bg-gradient-to-tl from-blue-700 to-purple-700 pt-32">
             <h1 className="text-6xl flex justify-center text-white font-mono tracking-widest">TRAINING DIARY</h1>
-            <div className="place-items-center pt-16 pb-32 max-w-fit flex-wrap mx-auto max-sm:ml-8 flex justify-center max-sm:inline-block mt-32">
+            <div className="place-items-center pt-10 pb-32 max-w-fit flex-wrap mx-auto max-sm:ml-8 flex justify-center max-sm:inline-block">
                 {
-                    stravaData.map((item, key) => {
-                        return <StravaCard dataObj={item} key={key} />
+                    stravaData.map(item => {
+                        return <StravaCard obj={item} />
                     })
                 }
             </div>
