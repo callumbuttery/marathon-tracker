7ea45e0a6a959301bce3d67ddf40f4a896b1cafc
Callum Buttery <callumbuttery@icloud.com>
2023-06-18 18:58:05 +0100

    Feat: pass props into StravaCard for past events

--- src/pages/PastEvents.tsx
+++ src/pages/PastEvents.tsx
@@ -1,12 +1,22 @@
 import Icon from '@mdi/react';
 import { mdiFlagCheckered, mdiRun } from '@mdi/js';
 
+import { StravaCard } from '../components/sections/StravaCard';
+
 export const PastEvents: React.FC = () => {
+
+    const dataObj = {
+        name: 'Tough Mudder 2023',
+        distance: 17.62,
+        type: 'Event'
+    }
 
     return (
         <div>
             <div className="h-max bg-gradient-to-tl from-blue-700 to-sky-400 pt-32">
                 <h1 className="text-6xl flex justify-center text-white font-mono tracking-widest">PAST EVENTS</h1>
+
+                <StravaCard obj={dataObj}/>
             </div>
         </div>
     )
