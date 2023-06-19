531c733f517b1565440f15e17ec69d18a1246523
Callum Buttery <callumbuttery@icloud.com>
2023-06-19 17:05:59 +0100

    Fix: remove type

--- src/components/sections/StravaCard.tsx
+++ src/components/sections/StravaCard.tsx
@@ -36,7 +36,6 @@ export const StravaCard: React.FC<expectedProps> = (props) => {
                     <a href="#">
                         <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{props.obj.name}</h5>
                     </a>
-                    <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">{props.obj.type}</p>
                     {
                         displayDistance
                     }
