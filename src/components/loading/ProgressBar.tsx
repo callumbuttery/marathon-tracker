302f388b54d413851946d5627632bef8c059b084
Callum Buttery <callumbuttery@icloud.com>
2023-06-22 22:23:43 +0100

    Style: update landing page styles

--- src/components/loading/ProgressBar.tsx
+++ src/components/loading/ProgressBar.tsx
@@ -1,5 +1,5 @@
 import Icon from '@mdi/react';
-import { mdiPartyPopper, mdiEmoticonSadOutline  } from '@mdi/js';
+import { mdiPartyPopper, mdiEmoticonSadOutline } from '@mdi/js';
 import { getCharityData } from '../../lib/CharityHelpers';
 
 import { useEffect, useState } from 'react'
@@ -25,12 +25,18 @@ export const ProgressBar: React.FC = () => {
         <div className=''>
             <span className="flex justify-center items-center mt-10 font-mono text-xl pt-1 text-white">Total Raised</span>
             <div className="flex justify-center items-center">
-                <div className="font-mono text-xl text-white flex"><Icon path={mdiEmoticonSadOutline} size={1} className='mr-2 ml-2' color={'orange'}/>£0</div>
-                <div className="ml-5 mr-5 w-5/12 bg-gray-200 rounded-full dark:bg-gray-700">
-                    <div className="bg-gradient-to-r from-pink-500 via-red-500 to-yellow-500 text-xs font-medium text-black text-center p-0.5 leading-none rounded-full" style={{ width: percentageRaised + "%" }}> {percentageRaised}%</div>
+                <div className="font-mono text-xl text-white flex">
+                    {/* <Icon path={mdiEmoticonSadOutline} size={1} className='mr-2 ml-2' color={'white'} /> */}
+                    £0
                 </div>
-                <div className="font-mono text-xl text-white flex">£{target} <Icon path={mdiPartyPopper} size={1} className='ml-2 mr-2' color={'orange'}/></div>
+                <div className="ml-5 mr-5 w-5/12 bg-gray-200 rounded-full dark:bg-gray-700">
+                    <div className="bg-gradient-to-r from-purple-600 to-blue-500 text-xs font-medium text-white text-center p-0.5 leading-none rounded-full" style={{ width: percentageRaised + "%" }}> {percentageRaised}%</div>
+                </div>
+                <div className="font-mono text-xl text-white flex">£{target}
+                    {/* <Icon path={mdiPartyPopper} size={1} className='ml-2 mr-2' color={'white'} /> */}
+                </div>
             </div>
+            <span className="flex justify-center items-center font-mono text-xl  text-white">For Cancer Research UK</span>
         </div>
     )
 }
