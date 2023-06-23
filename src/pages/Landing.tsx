4710886dfca6d87cfa956708914bbbf03c763b07
Callum Buttery <callumbuttery@icloud.com>
2023-06-23 09:42:20 +0100

    Style: change color

--- src/pages/Landing.tsx
+++ src/pages/Landing.tsx
@@ -11,7 +11,7 @@ export const Landing: React.FC = () => {
     return (
         <div>
             <div className="h-screen bg-gradient-to-tl from-indigo-700 to-purple-700 pt-52">
-                <div className="w-6/12 mx-auto rounded-2xl pb-2 pt-1 bg-gradient-to-br from-orange-500 to-red-500 drop-shadow-2xl">
+                <div className="w-6/12 mx-auto rounded-2xl pb-2 pt-1 bg-gradient-to-br from-orange-500 to-red-600 drop-shadow-2xl">
                     <CountDown />
                     <div className="flex justify-center items-center font-mono text-xl mt-12 text-white">
                         <Icon path={mdiRun} size={1} color={'white'} />
