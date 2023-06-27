e37558f624bb3f72dc055f82ed5c4eb722523224
Callum Buttery <callumbuttery@icloud.com>
2023-06-27 14:40:40 +0100

    Feat: make countdown mobile friendly

--- src/components/CountDown.tsx
+++ src/components/CountDown.tsx
@@ -17,34 +17,34 @@ export const CountDown: React.FC = () => {
     });
 
     return (
-        <div className="flex justify-center items-center mt-10 mb-10">
-            <div className="grid grid-flow-col gap-5 text-center auto-cols-max">
+        <div className="flex justify-center items-center mt-10 mb-10 ">
+            <div className="grid grid-flow-col gap-5 text-center auto-cols-max max-md:gap-1">
                 <div className="flex flex-col p-2 bg-neutral rounded-box text-neutral-content">
-                    <span className="countdown font-teko text-8xl text-white">
+                    <span className="countdown font-teko text-8xl text-white max-md:text-2xl">
                         <span>{Months}</span>
                     </span>
-                    <span className='text-black font-teko'>Months</span>
+                    <span className='text-black font-teko max-md:text-sm'>Months</span>
                 </div>
-                <div className='flex justify-center mt-5 text-6xl font-teko'>:</div>
+                <div className='flex justify-center mt-5 text-6xl font-teko max-md:text-2xl'>:</div>
                 <div className="flex flex-col p-2 bg-neutral rounded-box">
-                    <span className="countdown font-teko text-8xl text-white">
+                    <span className="countdown font-teko text-8xl text-white max-md:text-2xl">
                         <span>{days}</span>
                     </span>
-                    <span className='text-black font-teko'>Days</span>
+                    <span className='text-black font-teko max-md:text-sm'>Days</span>
                 </div>
-                <div className='flex justify-center mt-5 text-6xl font-teko'>:</div>
+                <div className='flex justify-center mt-5 text-6xl font-teko max-md:text-2xl'>:</div>
                 <div className="flex flex-col p-2 bg-neutral rounded-box text-neutral-content">
-                    <span className="countdown font-teko text-8xl text-white">
+                    <span className="countdown font-teko text-8xl text-white max-md:text-2xl">
                         <span>{hours}</span>
                     </span>
-                    <span className='text-black font-teko'>Hours</span>
+                    <span className='text-black font-teko max-md:text-sm'>Hours</span>
                 </div>
-                <div className='flex justify-center mt-5 text-6xl font-teko'>:</div>
+                <div className='flex justify-center mt-5 text-6xl font-teko max-md:text-2xl'>:</div>
                 <div className="flex flex-col p-2 bg-neutral rounded-box text-neutral-content">
-                    <span className="countdown font-teko text-8xl text-white">
+                    <span className="countdown font-teko text-8xl text-white max-md:text-2xl">
                         <span>{minutes}</span>
                     </span>
-                    <span className='text-black font-teko'>Minutes</span>
+                    <span className='text-black font-teko max-md:text-sm'>Minutes</span>
                 </div>
             </div>
         </div>
