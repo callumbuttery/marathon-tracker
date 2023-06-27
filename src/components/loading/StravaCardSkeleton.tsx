93e7f463020e3db0d74cc2830e916a6688dfab7f
Callum Buttery <callumbuttery@icloud.com>
2023-06-27 11:39:31 +0100

    Feat: add margins onto bottom of loading skeleton to prevent whitespace

--- src/components/loading/StravaCardSkeleton.tsx
+++ src/components/loading/StravaCardSkeleton.tsx
@@ -1,7 +1,7 @@
 export const StravaCardSkeleton: React.FC = () => {
     return (
         <div>
-            <div className="mx-auto bg-white shadow-2xl w-96 rounded-2xl animate-pulse mt-16">
+            <div className="mx-auto bg-white shadow-2xl w-96 rounded-2xl animate-pulse mt-16 mb-48">
                 <div className="h-48 p-3 overflow-hidden bg-gray-200 animate-pulse rounded-2xl">
                 </div>
                 <div className="p-3">
