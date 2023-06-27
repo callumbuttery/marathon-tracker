21973968abaec6abf1a1eb8f3a9cb1742da78b65
Callum Buttery <callumbuttery@icloud.com>
2023-06-27 16:51:25 +0100

    Feat: mobile responsive stats cards

--- src/components/sections/StatsCard.tsx
+++ src/components/sections/StatsCard.tsx
@@ -5,7 +5,7 @@ import { StatsCardSubSestion } from './StatsCardSubSection';
 export const StatsCard: React.FC<StatisticsInterface> = (props) => {
     return (
         <div className=''>
-            <div className='mx-auto flex grid grid-cols-2 gap-0 w-7/12 mt-24 drop-shadow-2xl gap-10 font-teko'>
+            <div className='mx-auto flex grid grid-cols-2 gap-0 w-7/12 mt-24 drop-shadow-2xl gap-10 font-teko max-md:grid-cols-1 max-md:w-10/12'>
                 <StatsCardSubSestion statsData={props.exerciseData.recentRunTotals} name="Recent Runs"/>
                 <StatsCardSubSestion statsData={props.exerciseData.all_run_totals} name="All Runs"/>
             </div>
