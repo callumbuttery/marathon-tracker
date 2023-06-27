bb33bb9ab3032b013d0568775ff7db7c14344bda
Callum Buttery <callumbuttery@icloud.com>
2023-06-27 17:35:53 +0100

    Fix: revert logic

--- src/components/CountDown.tsx
+++ src/components/CountDown.tsx
@@ -3,62 +3,60 @@ import moment from 'moment';
 
 export const CountDown: React.FC = () => {
 
-    const [timeRemaining, setTimeRemaining] = useState({
-        months: 0,
-        days: 0,
-        hours: 0,
-        minutes: 0,
-        seconds: 0,
-    });
+    const [seconds, setSeconds] = useState(0);
+    const [minutes, setMinutes] = useState(0);
+    const [hours, setHours] = useState(0);
+    const [days, setDays] = useState(0);
+    const [months, setMonths] = useState(0);
+
 
     useEffect(() => {
         const time = moment("2023-09-24").valueOf() - moment(new Date()).valueOf();
-        setTimeRemaining({ ...timeRemaining,
-            months: moment.duration(time).months(),
-            days: moment.duration(time).days(), 
-            hours: moment.duration(time).hours(),
-            minutes: moment.duration(time).minutes(),
-            seconds: moment.duration(time).seconds(),
-        })
+        //setSeconds(moment.duration(time).seconds());
+        setMinutes(moment.duration(time).minutes());
+        setHours(moment.duration(time).hours());
+        setDays(moment.duration(time).days());
+        setMonths(moment.duration(time).months());
     });
 
+    
     return (
         <div className="flex justify-center items-center mt-10 mb-10 ">
-            <div className="grid grid-flow-col gap-5 text-center auto-cols-max max-md:gap-1">
-                <div className="flex flex-col p-2 bg-neutral rounded-box text-neutral-content">
-                    <span className="countdown font-teko text-8xl text-white max-md:text-2xl">
-                        <span>{timeRemaining.months}</span>
+            <div className="grid grid-flow-col gap-2 text-center auto-cols-max max-md:gap-1">
+                <div className="flex flex-col p-2 rounded-box text-neutral-content">
+                    <span className="bg-white rounded-xl w-24 font-teko text-8xl text-black max-md:text-2xl">
+                        <span>{months}</span>
                     </span>
-                    <span className='text-black font-teko max-md:text-sm'>Months</span>
+                    <span className='text-black font-teko max-md:text-sm mt-2'>Months</span>
                 </div>
                 <div className='flex justify-center mt-5 text-6xl font-teko max-md:text-2xl'>:</div>
                 <div className="flex flex-col p-2 bg-neutral rounded-box">
-                    <span className="countdown font-teko text-8xl text-white max-md:text-2xl">
-                        <span>{timeRemaining.days}</span>
+                    <span className="bg-white rounded-xl w-24 font-teko text-8xl text-black max-md:text-2xl">
+                        <span>{days}</span>
                     </span>
-                    <span className='text-black font-teko max-md:text-sm'>Days</span>
+                    <span className='text-black font-teko max-md:text-sm mt-2'>Days</span>
                 </div>
                 <div className='flex justify-center mt-5 text-6xl font-teko max-md:text-2xl'>:</div>
                 <div className="flex flex-col p-2 bg-neutral rounded-box text-neutral-content">
-                    <span className="countdown font-teko text-8xl text-white max-md:text-2xl">
-                        <span>{timeRemaining.hours}</span>
+                    <span className="bg-white rounded-xl w-24 font-teko text-8xl text-black max-md:text-2xl">
+                        <span>{hours}</span>
                     </span>
-                    <span className='text-black font-teko max-md:text-sm'>Hours</span>
+                    <span className='text-black font-teko max-md:text-sm mt-2'>Hours</span>
                 </div>
                 <div className='flex justify-center mt-5 text-6xl font-teko max-md:text-2xl'>:</div>
                 <div className="flex flex-col p-2 bg-neutral rounded-box text-neutral-content">
-                    <span className="countdown font-teko text-8xl text-white max-md:text-2xl">
-                        <span>{timeRemaining.minutes}</span>
+                    <span className="bg-white rounded-xl w-24 font-teko text-8xl text-black max-md:text-2xl">
+                        <span>{minutes}</span>
                     </span>
-                    <span className='text-black font-teko max-md:text-sm'>Minutes</span>
+                    <span className='text-black font-teko max-md:text-sm mt-2'>Minutes</span>
                 </div>
-                <div className='flex justify-center mt-5 text-6xl font-teko max-md:text-2xl'>:</div>
+                {/* <div className='flex justify-center mt-5 text-6xl font-teko max-md:text-2xl'>:</div>
                 <div className="flex flex-col p-2 bg-neutral rounded-box text-neutral-content">
-                    <span className="countdown font-teko text-8xl text-white max-md:text-2xl">
-                        <span>{timeRemaining.seconds}</span>
+                    <span className="bg-white rounded-xl w-24 font-teko text-8xl text-black max-md:text-2xl">
+                        <span>{seconds}</span>
                     </span>
-                    <span className='text-black font-teko max-md:text-sm'>Seconds</span>
-                </div>
+                    <span className='text-black font-teko max-md:text-sm mt-2'>Seconds</span>
+                </div> */}
             </div>
         </div>
     )
