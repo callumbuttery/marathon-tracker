d4f5d2bfe8b64667e7480a0e36ed84121602bd09
Callum Buttery <callumbuttery@icloud.com>
2023-06-28 14:08:29 +0100

    Fix: revert ui changes

--- src/components/CountDown.tsx
+++ src/components/CountDown.tsx
@@ -24,35 +24,35 @@ export const CountDown: React.FC = () => {
         <div className="flex justify-center items-center mt-10 mb-10 ">
             <div className="grid grid-flow-col gap-2 text-center auto-cols-max max-md:gap-1">
                 <div className="flex flex-col p-2 rounded-box text-neutral-content">
-                    <span className="bg-white rounded-xl w-24 font-teko text-8xl text-black max-md:text-2xl">
+                    <span className="font-teko text-8xl text-white max-md:text-2xl">
                         <span>{months}</span>
                     </span>
                     <span className='text-black font-teko max-md:text-sm mt-2'>Months</span>
                 </div>
                 <div className='flex justify-center mt-5 text-6xl font-teko max-md:text-2xl'>:</div>
                 <div className="flex flex-col p-2 bg-neutral rounded-box">
-                    <span className="bg-white rounded-xl w-24 font-teko text-8xl text-black max-md:text-2xl">
+                    <span className="font-teko text-8xl text-white max-md:text-2xl">
                         <span>{days}</span>
                     </span>
                     <span className='text-black font-teko max-md:text-sm mt-2'>Days</span>
                 </div>
                 <div className='flex justify-center mt-5 text-6xl font-teko max-md:text-2xl'>:</div>
                 <div className="flex flex-col p-2 bg-neutral rounded-box text-neutral-content">
-                    <span className="bg-white rounded-xl w-24 font-teko text-8xl text-black max-md:text-2xl">
+                    <span className="font-teko text-8xl text-white max-md:text-2xl">
                         <span>{hours}</span>
                     </span>
                     <span className='text-black font-teko max-md:text-sm mt-2'>Hours</span>
                 </div>
                 <div className='flex justify-center mt-5 text-6xl font-teko max-md:text-2xl'>:</div>
                 <div className="flex flex-col p-2 bg-neutral rounded-box text-neutral-content">
-                    <span className="bg-white rounded-xl w-24 font-teko text-8xl text-black max-md:text-2xl">
+                    <span className="font-teko text-8xl text-white max-md:text-2xl">
                         <span>{minutes}</span>
                     </span>
                     <span className='text-black font-teko max-md:text-sm mt-2'>Minutes</span>
                 </div>
                 {/* <div className='flex justify-center mt-5 text-6xl font-teko max-md:text-2xl'>:</div>
                 <div className="flex flex-col p-2 bg-neutral rounded-box text-neutral-content">
-                    <span className="bg-white rounded-xl w-24 font-teko text-8xl text-black max-md:text-2xl">
+                    <span className=" font-teko text-8xl text-black max-md:text-2xl">
                         <span>{seconds}</span>
                     </span>
                     <span className='text-black font-teko max-md:text-sm mt-2'>Seconds</span>
