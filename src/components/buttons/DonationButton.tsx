d16aa6153c1afffedcfb6a272599561932fb1750
Callum Buttery <callumbuttery@icloud.com>
2023-07-12 15:54:08 +0100

    Fix: button width

--- src/components/buttons/DonationButton.tsx
+++ src/components/buttons/DonationButton.tsx
@@ -1,7 +1,7 @@
 export const DonationButton: React.FC = () => {
     return (
         <div>
-            <button onClick={() => window.open('https://www.justgiving.com/page/callum-buttery-1686254435318?utm_source=copyLink&utm_medium=one_page&utm_content=page/callum-buttery-1686254435318&utm_campaign=pfp-share&utm_term=9e800a73878d42298fd8d468b21fbeda', "_blank")} type="button" className="shadow-4xl text-white bg-gradient-to-br from-purple-600 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2 animate-bounce max-md:text-xs max-md:w-16 max-md:h-12">Donate!</button>
+            <button onClick={() => window.open('https://www.justgiving.com/page/callum-buttery-1686254435318?utm_source=copyLink&utm_medium=one_page&utm_content=page/callum-buttery-1686254435318&utm_campaign=pfp-share&utm_term=9e800a73878d42298fd8d468b21fbeda', "_blank")} type="button" className="shadow-4xl text-white bg-gradient-to-br from-purple-600 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2 animate-bounce max-md:text-xs max-md:w-24 max-md:h-12">Donate here!</button>
         </div>
     )
 }
