c72222ab045679f7dbb44a3b34320452a4ffde9b
Callum Buttery <callumbuttery@icloud.com>
2023-07-13 10:00:34 +0100

    Fix: movile stuff

--- src/components/loading/StravaCardSkeleton.tsx
+++ src/components/loading/StravaCardSkeleton.tsx
@@ -1,7 +1,7 @@
 export const StravaCardSkeleton: React.FC = () => {
     return (
         <div className="">
-            <div className="mx-auto bg-white shadow-2xl w-96 rounded-2xl animate-pulse mt-16 mb-8 max-md:w-72">
+            <div className="mx-auto bg-white shadow-2xl w-96 rounded-2xl animate-pulse mt-16 mb-8 max-md:w-72 max-md:flex max-md:justify-center">
                 <div className="h-48 p-3 overflow-hidden bg-gray-200 animate-pulse rounded-2xl">
                 </div>
                 <div className="p-3">
